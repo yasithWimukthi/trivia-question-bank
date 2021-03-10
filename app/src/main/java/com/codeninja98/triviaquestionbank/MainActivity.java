74779be27ea99c4115763ce02ec880d0eb662765
yasithWimukthi <it19966922@my.sliit.lk>
2021-03-10 11:52:29 +0530

    Implement QuestionBank

--- app/src/main/java/com/codeninja98/triviaquestionbank/MainActivity.java
+++ app/src/main/java/com/codeninja98/triviaquestionbank/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.codeninja98.triviaquestionbank.controller.AppController;
+import com.codeninja98.triviaquestionbank.data.QuestionBank;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        new QuestionBank().getQuestions();
 
     }
 }
