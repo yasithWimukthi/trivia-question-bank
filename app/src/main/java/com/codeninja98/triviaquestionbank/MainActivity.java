6274ebbcee8666e607376427e67f40735e4b5a1e
yasithWimukthi <it19966922@my.sliit.lk>
2021-03-11 12:59:27 +0530

    create interfaces

--- app/src/main/java/com/codeninja98/triviaquestionbank/MainActivity.java
+++ app/src/main/java/com/codeninja98/triviaquestionbank/MainActivity.java
@@ -1,7 +1,10 @@
 package com.codeninja98.triviaquestionbank;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 
@@ -15,7 +18,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+
+        // Define ColorDrawable object and parse color
+        // using parseColor method
+        // with color hash code as its parameter
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#008677"));
+
+        // Set BackgroundDrawable
+        actionBar.setBackgroundDrawable(colorDrawable);
+
         new QuestionBank().getQuestions();
+
 
     }
 }
