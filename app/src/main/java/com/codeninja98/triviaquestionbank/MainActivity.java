228b3b3574141c8ea98e5b2cf1188a3459fab49b
yasithWimukthi <it19966922@my.sliit.lk>
2021-03-13 09:26:08 +0530

    Implement AnswerListAsyncResponse interface

--- app/src/main/java/com/codeninja98/triviaquestionbank/MainActivity.java
+++ app/src/main/java/com/codeninja98/triviaquestionbank/MainActivity.java
@@ -7,30 +7,69 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.codeninja98.triviaquestionbank.controller.AppController;
+import com.codeninja98.triviaquestionbank.data.AnswerListAsyncResponse;
 import com.codeninja98.triviaquestionbank.data.QuestionBank;
+import com.codeninja98.triviaquestionbank.model.Question;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView questionTextView;
+    private TextView questionCounterTextView;
+    private Button trueButton;
+    private Button falseButton;
+    private ImageButton nextButton;
+    private ImageButton prevButton;
+    private int currentQuestionIndex;
+    private List<Question> questionList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // change app bar color
         ActionBar actionBar;
         actionBar = getSupportActionBar();
-
         // Define ColorDrawable object and parse color
         // using parseColor method
         // with color hash code as its parameter
         ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#008677"));
-
         // Set BackgroundDrawable
         actionBar.setBackgroundDrawable(colorDrawable);
 
-        new QuestionBank().getQuestions();
+        nextButton = findViewById(R.id.next_btn);
+        prevButton = findViewById(R.id.prev_btn);
+        falseButton = findViewById(R.id.false_btn);
+        trueButton = findViewById(R.id.true_btn);
+        questionTextView = findViewById(R.id.question_textview);
+        questionCounterTextView = findViewById(R.id.counterText);
 
+        nextButton.setOnClickListener(this);
+        prevButton.setOnClickListener(this);
+        falseButton.setOnClickListener(this);
+        trueButton.setOnClickListener(this);
+
+        questionList = new QuestionBank().getQuestions(new AnswerListAsyncResponse() {
+            @Override
+            public void processFinished(ArrayList<Question> questionArrayList) {
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
